8dbe726b11a72781a7af415774703f1d6ad5ce2a
Stefan <stefan.joncic@united.group>
2022-05-13 16:30:49 +0200

    service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Job } from '../model/job.model';
 
 
 @Injectable({
@@ -9,13 +10,18 @@ import { BehaviorSubject } from 'rxjs';
 export class ApiService {
 
   public search = new BehaviorSubject<string>("");
+  public location = new BehaviorSubject<any>([]);
 
   constructor(private http: HttpClient) { }
 
   getJobList(){
-    return this.http.get<any>(`http://localhost:3000/jobs`);
+    return this.http.get<Job>(`http://localhost:3000/jobs`);
   }
   getCurrentJobById(jobId: string){
-    return this.http.get<any>(`http://localhost:3000/jobs/${jobId}`);
+    return this.http.get<Job>(`http://localhost:3000/jobs/${jobId}`);
+  }
+
+  getJobsLocation(){
+    return this.http.get<any>(`http://localhost:3000/jobs`);
   }
 }
